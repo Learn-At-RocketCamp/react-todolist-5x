6878d95921181d612bcdda528bd4f19f2f2b6473
Aya <90342033+Aya-X@users.noreply.github.com>
2022-07-18 16:07:10 +0800

    chore: Add ESLint with airbnb

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+import React, { useState } from 'react';
+
+import logo from './logo.svg';
+import poweredBy from './powered-by-vitawind-dark.png';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,29 +16,35 @@ function App() {
         />
         <style>
           {
-            "\
+            '\
             .animate-speed{\
               animation-duration:20s;\
             }\
-          "
+          '
           }
         </style>
+
         <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
           Vite + React + Tailwindcss v3
         </p>
+
         <p className="mt-3">
           <button
             type="button"
             className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
+            onClick={() => setCount((prevCount) => prevCount + 1)}
           >
-            count is: {count}
+            count is:
+            {count}
           </button>
         </p>
+
         <p>
-          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test
-          HMR updates.
+          Edit
+          <code className="text-[#8d96a7]">App.jsx</code>
+          and save to test HMR updates.
         </p>
+
         <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
           <a
             className="text-[#61dafb] transition-all hover:text-blue-400"
@@ -47,7 +54,8 @@ function App() {
           >
             Learn React
           </a>
-          {" | "}
+          {' | '}
+
           <a
             className="text-[#61dafb] transition-all hover:text-blue-400"
             href="https://vitejs.dev/guide/features.html"
@@ -57,6 +65,7 @@ function App() {
             Vite Docs
           </a>
         </p>
+
         <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
       </header>
     </div>
