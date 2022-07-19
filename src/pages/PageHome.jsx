07c6dc1046cab846ed6a8c739514212abad1ecbe
Aya <90342033+Aya-X@users.noreply.github.com>
2022-07-19 18:48:08 +0800

    refactor: Update pages fn to folder
    Update pages folder:
    - export functions

---
+++ src/pages/PageHome.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+/**
+ * #XXX: ESLint
+ * 'React' must be in scope when using JSX
+ */
+
+function PageHome() {
+  // function PageHome() {
+  return (
+    <>
+      <h3>PAGE-Home</h3>
+
+      <hr />
+    </>
+  );
+}
+
+// const PageHome = () => {
+// #NOTE: `Function component is not a function declaration.`
+//   return <h3>Home</h3>;
+// };
+// const PageHome = () => <h3>Home</h3>;
+
+export default PageHome;
