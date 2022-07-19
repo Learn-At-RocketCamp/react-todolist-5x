fc8b86ed3166fe5dfef66f0830f0e59ee05039b7
Aya <90342033+Aya-X@users.noreply.github.com>
2022-07-19 12:27:58 +0800

    feat: Add "useRoutes" to App
    Add "useRoutes()" to wrap "Routes":
    
    1. routes config element
    2. pages function
    3. "Layout" with nav:
      - "Link" to "path"
      - "Outlet" to display "element"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,96 @@
 import React, { useState } from 'react';
+import { useRoutes, Link, Outlet } from 'react-router-dom';
 
 import logo from './logo.svg';
 import poweredBy from './powered-by-vitawind-dark.png';
 
+function PageHome() {
+  return (
+    <main>
+      <h3>PAGE-Home</h3>
+    </main>
+  );
+}
+
+function PageTodos() {
+  return (
+    <main>
+      <h3>PAGE-Todos</h3>
+    </main>
+  );
+}
+
+// const PageHome = () => {
+// #NOTE: `Function component is not a function declaration.`
+//   return <h3>Home</h3>;
+// };
+// const PageHome = () => <h3>Home</h3>;
+
+function PageNotFound() {
+  return (
+    <main>
+      <h3>404</h3>
+      <p>PAGE-NOT-FOUND</p>
+    </main>
+  );
+}
+/* end of Pages-fn() */
+
+function Layout() {
+  return (
+    <>
+      <nav>
+        <ul className="text-sky-600">
+          <li>
+            <Link to="/">HOME</Link>
+            {/*
+             * #XXX:
+             * A control must be associated with a text label.
+             * The href attribute requires a valid value to be accessible.
+             * <a href=""></a>
+             */}
+          </li>
+          <li>
+            <Link to="/todos">TODOS</Link>
+          </li>
+          <li>
+            <Link to="/idontknowwheretogo">404</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr className="h-2 bg-gray-300" />
+
+      <Outlet />
+    </>
+  );
+}
+/* end of App-Layout() */
+
 function App() {
   const [count, setCount] = useState(0);
 
+  /**
+   * #NOTE: useRoutes() === Hook
+   * Hooks can only be called inside of the body of a function component.
+   */
+  const routesConfig = useRoutes([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { index: true, element: <PageHome /> },
+        { path: '/todos', element: <PageTodos /> },
+        { path: '*', element: <PageNotFound /> },
+      ],
+    },
+  ]);
+  /* end of useRoutes([]) */
+
   return (
     <div className="text-center selection:bg-green-900">
+      {routesConfig}
+
       <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
         <img
           src={logo}
